7cb5c5247490ba122bf83c358cef7721302f11bd
fqaiser94 <fqaiser94@gmail.com>
2017-11-18 21:14:41 -0500

    optimize only for fc8 vars

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -66,7 +66,7 @@ rate = 0.001
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation)
+training_operation = optimizer.minimize(loss_operation, var_list=[fc8W, fc8b])
 
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
